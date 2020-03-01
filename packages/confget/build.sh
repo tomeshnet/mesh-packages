27247b8e747ac9562b1bd1ae2fb874d23232dcfe
darkdrgn2k <darkdrgn2k@users.noreply.github.com>
2020-02-29 21:05:09 -0500

    Build script for confget

---
+++ packages/confget/build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+case "$ARCH" in
+  all)
+    PKG_ARCH="all"
+  ;;
+  *)
+    exit 0
+  ;;
+esac
+
+# Prep working directory
+mkdir root
+root=$(pwd)
+
+# Prepare root directory
+cp -R files/* root/
+
+# define version number
+version=0.0.1
+
+# Make deb pacakges
+echo "Version: $version" >> root/DEBIAN/control
+echo Architecture: $ARCH >> root/DEBIAN/control
+
+sudo chown -R root.root root
+dpkg-deb --build root
+sudo rm -rf root
+mv root.deb ../confget-$version-$ARCH.deb
