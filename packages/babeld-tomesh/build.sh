26962e96e7606eafed10d9dcc4aaae178c3a5a0d
darkdrgn2k <darkdrgn2k@users.noreply.github.com>
2020-08-14 11:09:44 -0400

    No really, add babeld

---
+++ packages/babeld-tomesh/build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+case "$ARCH" in
+  all)
+    PKG_ARCH="all"
+  ;;
+  *)
+    exit 0
+  ;;
+esac
+
+# Prep working directory
+mkdir root
+mkdir tmp
+root=$(pwd)
+
+# Prepare root directory
+cp -R files/* root/
+chmod 755 root/DEBIAN/postinst
+
+version=1
+
+# Make deb pacakges
+echo "Version: $version" >> root/DEBIAN/control
+echo Architecture: $ARCH >> root/DEBIAN/control
+
+sudo chown -R root.root root
+dpkg-deb --build root
+sudo rm -rf root
+mv root.deb ../babeld-tomesh-$version-$ARCH.deb
+
+# Install and cleanup
+rm -rf tmp
